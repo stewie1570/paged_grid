2e64b7b397ea06ac8c74569e0d7de6b31dacd49c
Anderson <23005@aruplab.net>
2017-04-18 14:48:51 -0600

    Re-indented App.js

--- src/App.js
+++ src/App.js
@@ -9,44 +9,44 @@ import 'bootstrap-css-only'
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.rows = _.range(700).map(i => ({
-      col1: `row #${i}`,
-      col2: `row #${i}`
-    }));
-    this.state = {
-      currentPage: 0,
-      filter: ""
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.rows = _.range(700).map(i => ({
+            col1: `row #${i}`,
+            col2: `row #${i}`
+        }));
+        this.state = {
+            currentPage: 0,
+            filter: ""
+        };
+    }
 
-  render() {
-    var numPerPage = 6;
-    var filteredData = Filter.on({
-      data: this.rows,
-      onColumns: ["col1", "col2"],
-      value: this.state.filter
-    });
+    render() {
+        var numPerPage = 6;
+        var filteredData = Filter.on({
+            data: this.rows,
+            onColumns: ["col1", "col2"],
+            value: this.state.filter
+        });
 
-    return <div>
-      <TextInput onChange={filter => this.setState({ filter })} />
-      <HorozontalGrid
-        rows={Paging
-          .on(filteredData)
-          .getPage({ numPerPage, page: this.state.currentPage })
-        }
-        columns={["col1", "col2"]}
-        noRowsMessage={<i>No data...</i>}
-        className="table table-striped" />
-      <Pager
-        rows={filteredData}
-        numPerPage={numPerPage}
-        maxPages={5}
-        currentPage={this.state.currentPage}
-        onPageSelected={currentPage => this.setState({ currentPage })} />
-    </div>;
-  }
+        return <div>
+            <TextInput onChange={filter => this.setState({ filter })} />
+            <HorozontalGrid
+                rows={Paging
+                    .on(filteredData)
+                    .getPage({ numPerPage, page: this.state.currentPage })
+                }
+                columns={["col1", "col2"]}
+                noRowsMessage={<i>No data...</i>}
+                className="table table-striped" />
+            <Pager
+                rows={filteredData}
+                numPerPage={numPerPage}
+                maxPages={5}
+                currentPage={this.state.currentPage}
+                onPageSelected={currentPage => this.setState({ currentPage })} />
+        </div>;
+    }
 }
 
 export default App;
